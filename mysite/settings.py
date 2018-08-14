04e0435cd7a33629cf2ebae7a9d7625702d53aac
Ítalo Lemos <italo.lemos@reitoria.ifpe.edu.br>
2018-08-14 09:30:05 -0300

    Second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%ymny-!plsi=7pq)lqqwh-8+(q9(33z-4ut_d4uw(*o2^syadc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web']
 
 
 # Application definition
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= '/static'
--- run_web.sh
+++ run_web.sh
@@ -2,5 +2,5 @@
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic --noinput
-python3 manage.py runserver 0.0.0.0:8000
+python manage.py collectstatic --no-input
+gunicorn mysite.wsgi -b 0.0.0.0:8000
